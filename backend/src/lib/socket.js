ce508fb76fa76d92e518ff5248a678acecc660fc
Aditya02git <m.adityamondal2003@gmail.com>
2025-10-27 23:48:57 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chattrix-app-g8al.onrender.com"],//"http://localhost:5173" "https://chattrix-app-g8al.onrender.com"
+    origin: ["https://chattrix-app-g8al.onrender.com"], // ✅ Your frontend URL
+    credentials: true,
   },
 });
 
@@ -20,13 +21,10 @@ const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
-
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
+  
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
