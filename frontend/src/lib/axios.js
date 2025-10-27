da344097075da3c578abc3b14ef05935a72299d0
Aditya02git <m.adityamondal2003@gmail.com>
2025-10-27 16:11:30 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://chattrix-l0cr.onrender.com",
     withCredentials: true,
 });
